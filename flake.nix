5e7cb415aa656c85fe042b1c79b386efe862a7cc
Smaug123 <patrick+github@patrickstevens.co.uk>
2023-09-30 00:48:28 +0100

    Extract shell script

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     flake-utils.url = github:numtide/flake-utils;
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     anki-compiler.url = "github:Smaug123/anki-dotnet";
+    scripts.url = "github:Smaug123/flake-shell-script";
   };
 
   outputs = {
@@ -12,6 +13,7 @@
     nixpkgs,
     flake-utils,
     anki-compiler,
+    scripts,
   }: let
     commit = self.shortRev or "dirty";
     date = self.lastModifiedDate or self.lastModified or "19700101";
@@ -19,38 +21,6 @@
   in
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
-    in let
-      createShellScript = name: contents:
-        pkgs.stdenv.mkDerivation {
-          __contentAddressed = true;
-          pname = name;
-          version = "0.1.0";
-          src = contents;
-
-          buildInputs = [
-            pkgs.shellcheck
-          ];
-
-          phases = ["configurePhase" "buildPhase" "installPhase"];
-
-          configurePhase = ''
-            ${pkgs.shellcheck}/bin/shellcheck "${contents}"
-          '';
-
-          buildPhase = ''
-            cp "${contents}" run.sh
-            patchShebangs run.sh
-            sed -i 's_"/bin/bash"_"${pkgs.bash}/bin/bash"_' run.sh
-            sed -i 's_"/bin/sh"_"${pkgs.bash}/bin/bash"_' run.sh
-          '';
-
-          installPhase = ''
-            mkdir -p $out
-            mv run.sh $out/run.sh
-          '';
-        };
-    in let
-      buildAnki = createShellScript "anki" ./build.sh;
     in {
       packages.default = pkgs.stdenv.mkDerivation {
         __contentAddressed = true;
@@ -59,7 +29,7 @@
         src = ./.;
         buildInputs = [];
         installPhase = ''
-          ${buildAnki}/run.sh . "${anki-compiler.packages.${system}.default}/bin/AnkiStatic" "$out"
+          ${scripts.lib.createShellScript pkgs "anki" ./build.sh}/run.sh . "${anki-compiler.packages.${system}.default}/bin/AnkiStatic" "$out"
         '';
       };
       devShells.default = pkgs.mkShell {
